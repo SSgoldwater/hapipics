1c245f02d22bf795f12cd1f6c1b2fe5766cadccf
Stanley <stanleyjsdz@gmail.com>
2015-10-27 07:26:28 -0600

    add method in controller for instagram callback

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,4 +3,10 @@ class OauthController < ApplicationController
   def connect
     redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
   end
+
+  def callback
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    session[:access_token] = response.access_token
+    redirect_to root_path
+  end
 end
