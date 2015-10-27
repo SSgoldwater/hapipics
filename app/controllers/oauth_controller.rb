fd8f0e9e522110019522f5a9d10c559b65478d69
Stanley <stanleyjsdz@gmail.com>
2015-10-27 07:51:31 -0600

    change oauth callback for heroku

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,5 +1,5 @@
 class OauthController < ApplicationController
-  CALLBACK_URL = "http://127.0.0.1:3000/oauth/callback/"
+  CALLBACK_URL = "http://hapipics.herokuapp.com/oauth/callback/"
   def connect
     redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
   end
