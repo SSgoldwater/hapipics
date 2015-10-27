a636334e5ff8e0433efe34819b824adc26295cf7
Stanley <stanleyjsdz@gmail.com>
2015-10-27 14:22:53 -0600

    creates a user hash in the auth controller callback method

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -9,6 +9,7 @@ class OauthController < ApplicationController
   def callback
     response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
     session[:access_token] = response.access_token
+    session[:user] = response.user
     redirect_to user_path
   end
 
