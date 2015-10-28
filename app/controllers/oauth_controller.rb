36f1cbc2f5ec245ab711343db4e69b7f74123602
Stanley <stanleyjsdz@gmail.com>
2015-10-27 19:26:49 -0600

    added callback_url to application.yml

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,13 +1,10 @@
 class OauthController < ApplicationController
-  CALLBACK_URL = "http://hapipics.herokuapp.com/oauth/callback/"
-  #CALLBACK_URL = "http://127.0.0.1:3000/oauth/callback/"
-
   def connect
-    redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+    redirect_to Instagram.authorize_url(:redirect_uri => ENV["callback_url"])
   end
 
   def callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    response = Instagram.get_access_token(params[:code], :redirect_uri => ENV["callback_url"])
     session[:access_token] = response.access_token
     session[:user] = response.user
     redirect_to user_path
