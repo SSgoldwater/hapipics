4698a1e6e25f29a2de893261735a710316e00381
Stanley <stanleyjsdz@gmail.com>
2015-10-28 14:18:37 -0600

    add feed object to show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     token = session[:access_token]
     @user = User.find('self', token).data
+    @feed = UserFeed.find('self', token).data
   end
 end
