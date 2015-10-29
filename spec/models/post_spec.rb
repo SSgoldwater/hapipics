265122109341e81189a0aeb1b50c7908b351dffa
Stanley <stanleyjsdz@gmail.com>
2015-10-29 14:47:26 -0600

    add hapitag feed spec, change cassette data expectations in post spec, finish user media spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,23 +9,23 @@ RSpec.describe Post, type: :model do
   end
 
   it "has a poster_pic method that returns poster pic" do
-    expect(@post.poster_pic).to eq('https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-19/s150x150/11251648_877316879011322_1761317770_a.jpg')
+    expect(@post.poster_pic).to eq('https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xtf1/t51.2885-19/11117073_830991770320109_177291229_a.jpg')
   end
   
   it "has poster_name method that returns poster name" do
-    expect(@post.poster_name).to eq('landrover')
+    expect(@post.poster_name).to eq('deadmau5')
   end
 
   it "has an image method that returns the post image" do
-    expect(@post.image).to eq('https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s640x640/sh0.08/e35/12142078_771999726260532_988980953_n.jpg')
+    expect(@post.image).to eq('https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s640x640/sh0.08/e35/12145192_899956370084311_708524410_n.jpg')
   end
 
   it "has a post_caption method that returns post caption" do
-    expect(@post.poster_pic).to eq('https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-19/s150x150/11251648_877316879011322_1761317770_a.jpg')
+    expect(@post.poster_pic).to eq('https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xtf1/t51.2885-19/11117073_830991770320109_177291229_a.jpg')
   end
   
   it "has a like_count method that returns like count" do
-    expect(@post.like_count).to eq(3221)
+    expect(@post.like_count).to eq(3752)
   end
   
 end
