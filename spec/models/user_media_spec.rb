0d6d05193943db87ca0ba8fcf9c3c8e25a25af04
Stanley <stanleyjsdz@gmail.com>
2015-10-29 13:51:28 -0600

    model test for user_media

---
+++ spec/models/user_media_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe UserMedia, type: :model do
+
+  it "finds user media by self" do
+    VCR.use_cassette("usermedia#find") do
+      @feed = UserMedia.find('self', ENV['token'])
+    end
+  end
+
+end
